7c1863e47d6571391f673dbbd75aacfda7a6f24b
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2017-10-24 22:05:45 +0200

    Homepage button for book creation

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,25 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+        <div class="col-md-12">
+            @if (session('status'))
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
+            @endif
 
-                <div class="panel-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+            <div class="heading">
+                <h1>Books</h1>
+                @can('add books')
+                    <div>
+                        <a href="{{ route('books.create') }}" class="btn btn-default center-v">Add a new book &nbsp;<span style="margin-top: -1px" class="glyphicon glyphicon-plus-sign"></span></a>
+                    </div>
+                @endcan
+            </div>
 
-                    You are logged in!
+            <div class="container-fluid">
+                <div class="row">
+
                 </div>
             </div>
         </div>
