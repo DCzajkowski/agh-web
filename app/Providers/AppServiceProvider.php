c0d33ea6d0d29b1163d117da919a09906a60fe16
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2017-11-01 23:01:58 +0100

    Redirect unknown urls

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Routing\UrlGenerator;
+use Illuminate\Support\Facades\Request;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,6 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(UrlGenerator $url)
     {
+        if (Request::root() !== config('app.url')) {
+            return redirect(config('app.url'));
+        }
+
         if (config('app.env') !== 'local') {
             $url->forceScheme('https');
         }
