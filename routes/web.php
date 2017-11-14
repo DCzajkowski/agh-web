a7ebf2015680e85dcd596827cf632d8f54f3905a
Dariusz Czajkowski <dare.czajkowski@gmail.com>
2017-11-14 20:02:37 +0100

    Make routes auth-protected

--- routes/web.php
+++ routes/web.php
@@ -17,18 +17,20 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::get('/books/create', 'BooksController@create')->name('books.create');
-Route::post('/books', 'BooksController@store')->name('books.store');
-Route::get('/books/{book}/edit', 'BooksController@edit')->name('books.edit');
-Route::put('/books/{book}', 'BooksController@update')->name('books.update');
-Route::patch('/books/{book}', 'BooksController@update')->name('books.update');
-Route::delete('/books/{book}', 'BooksController@destroy')->name('books.destroy');
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/books/create', 'BooksController@create')->name('books.create');
+    Route::post('/books', 'BooksController@store')->name('books.store');
+    Route::get('/books/{book}/edit', 'BooksController@edit')->name('books.edit');
+    Route::put('/books/{book}', 'BooksController@update')->name('books.update');
+    Route::patch('/books/{book}', 'BooksController@update')->name('books.update');
+    Route::delete('/books/{book}', 'BooksController@destroy')->name('books.destroy');
 
-Route::get('/checkout', 'CheckoutsController@index')->name('checkouts.index');
-Route::get('/checkout/create/{bookId?}', 'CheckoutsController@create')->name('checkouts.create');
-Route::post('/checkout', 'CheckoutsController@store')->name('checkouts.store');
-Route::delete('/checkout/{bookId}', 'CheckoutsController@destroy')->name('checkouts.destroy');
+    Route::get('/checkout', 'CheckoutsController@index')->name('checkouts.index');
+    Route::get('/checkout/create/{bookId?}', 'CheckoutsController@create')->name('checkouts.create');
+    Route::post('/checkout', 'CheckoutsController@store')->name('checkouts.store');
+    Route::delete('/checkout/{bookId}', 'CheckoutsController@destroy')->name('checkouts.destroy');
 
-Route::get('/theme/{mode}', 'ThemeController@change')->name('theme.switch');
+    Route::get('/theme/{mode}', 'ThemeController@change')->name('theme.switch');
 
-Route::get('/chat', 'ChatController@index')->name('chat.index');
+    Route::get('/chat', 'ChatController@index')->name('chat.index');
+});
